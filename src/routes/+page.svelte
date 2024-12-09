cefd475122e68187e89ef395bb5d39cfd45abbde
vvvyyynet <63237439+vvvyyynet@users.noreply.github.com>
2024-12-09 14:22:49 +0100

    add comingsoon-page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,8 @@
 <script>
-	import { ProgressBar } from '@skeletonlabs/skeleton';
 </script>
 
-<div class="flex h-screen w-screen flex-col items-center justify-center gap-5">
-	<h1>vvvyyy.net</h1>
-	<ProgressBar class="mt-5 !w-24" value={undefined} />
-	<button type="button" class="variant-filled-primary btn btn-lg">
-		<span class="ml-[-5px]"><img class="h-8" src="/favicon.png" alt="" /></span>
-		<span>Button</span>
-	</button>
+<div
+	class="flex h-screen w-screen flex-col items-center justify-center gap-5 bg-gray-900 text-white"
+>
+	<h1 class="text-2xl font-bold">vvvyyy.net</h1>
 </div>
