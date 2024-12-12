fbf41c3d615dcd7e4c98be58e2f881b72dbba8d0
vvvyyynet <63237439+vvvyyynet@users.noreply.github.com>
2024-12-12 08:21:44 +0100

    adapt template to obsidian-table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,5 +4,5 @@
 <div
 	class="flex h-screen w-screen flex-col items-center justify-center gap-5 bg-gray-900 text-white"
 >
-	<h1 class="text-2xl font-bold">vvvyyy.net</h1>
+	<h1 class="text-2xl font-bold">obsidian table</h1>
 </div>
